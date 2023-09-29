ce2e5fb09eebb679a3d08bf84649b0a0ecf31df2
sersus <serghei.suschevici@gmail.com>
2023-09-29 15:55:10 +0300

    iter 1  try 1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,80 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+// MemStorage представляет хранилище метрик.
+type MemStorage struct {
+	metrics map[string]interface{}
+}
+
+// NewMemStorage создает новое хранилище метрик.
+func NewMemStorage() *MemStorage {
+	return &MemStorage{
+		metrics: make(map[string]interface{}),
+	}
+}
+
+// UpdateGauge обновляет метрику типа gauge.
+func (s *MemStorage) UpdateGauge(name string, value float64) {
+	s.metrics[name] = value
+}
+
+// UpdateCounter обновляет метрику типа counter.
+func (s *MemStorage) UpdateCounter(name string, value int64) {
+	if existingValue, ok := s.metrics[name]; ok {
+		if oldValue, isInt := existingValue.(int64); isInt {
+			s.metrics[name] = oldValue + value
+		} else {
+			// Возвращаем ошибку, если существует метрика другого типа с таким же именем.
+			panic(fmt.Sprintf("Metric %s is not a counter", name))
+		}
+	} else {
+		s.metrics[name] = value
+	}
+}
+
+func main() {
+	storage := NewMemStorage()
+
+	http.HandleFunc("/update/", func(w http.ResponseWriter, r *http.Request) {
+		parts := strings.Split(r.URL.Path, "/")
+		if len(parts) != 5 {
+			http.Error(w, "Invalid request", http.StatusBadRequest)
+			return
+		}
+
+		metricType := parts[2]
+		metricName := parts[3]
+		metricValueStr := parts[4]
+
+		switch metricType {
+		case "gauge":
+			metricValue, err := strconv.ParseFloat(metricValueStr, 64)
+			if err != nil {
+				http.Error(w, "Invalid metric value", http.StatusBadRequest)
+				return
+			}
+			storage.UpdateGauge(metricName, metricValue)
+		case "counter":
+			metricValue, err := strconv.ParseInt(metricValueStr, 10, 64)
+			if err != nil {
+				http.Error(w, "Invalid metric value", http.StatusBadRequest)
+				return
+			}
+			storage.UpdateCounter(metricName, metricValue)
+		default:
+			http.Error(w, "Invalid metric type", http.StatusBadRequest)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
+
