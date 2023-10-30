b18c0405e8b6bb29d7b82d0ccd68db6dca256b86
Suschevici <ss@1cmd.md>
2023-10-30 11:22:36 +0200

    iter6 try 1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"flag"
-	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/sersus/go-yandex-metrics/internal/handlers"
+	log "github.com/sersus/go-yandex-metrics/internal/logger"
+	"go.uber.org/zap"
 )
 
 var address = flag.String("a", "localhost:8080", "Server listening address")
 
 func main() {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+
+	log.SugarLogger = *logger.Sugar()
+
 	flag.Parse()
 	envAddr, exists := os.LookupEnv("ADDRESS")
 	if exists && envAddr != "" {
@@ -20,9 +29,17 @@ func main() {
 	}
 	metricsHandler := &handlers.MetricsHandler{}
 	r := chi.NewRouter()
+	r.Use(log.RequestLogger)
 	r.Post("/update/*", metricsHandler.SendMetric)
 	r.Get("/value/*", metricsHandler.GetMetric)
 	r.Get("/", metricsHandler.ShowMetrics)
 
-	log.Fatal(http.ListenAndServe(*address, r))
+	log.SugarLogger.Infow(
+		"Starting server",
+		"addr", *address,
+	)
+	if err := http.ListenAndServe(*address, r); err != nil {
+		// записываем в лог ошибку, если сервер не запустился
+		log.SugarLogger.Fatalw(err.Error(), "event", "start server")
+	}
 }
