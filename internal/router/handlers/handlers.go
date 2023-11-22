199698a039d092da834aed41ab0f1f97a1d1fcf8
Suschevici <ss@1cmd.md>
2023-11-22 23:54:24 +0200

    w2

--- internal/router/handlers/handlers.go
+++ internal/router/handlers/handlers.go
@@ -254,7 +254,8 @@ func (h *handler) GetMetric(w http.ResponseWriter, r *http.Request) {
 		}
 	case storage.Gauge:
 		//if _, err = io.WriteString(w, fmt.Sprintf("%.3f", *value.Value)); err != nil {
-		if _, err = io.WriteString(w, fmt.Sprintf("%g", *value.Value)); err != nil {
+		//if _, err = io.WriteString(w, fmt.Sprintf("%g", *value.Value)); err != nil {
+		if _, err = io.WriteString(w, fmt.Sprint(*value.Value)); err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
