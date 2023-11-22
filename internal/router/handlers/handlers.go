110224dbc818c3a16f64d5ce8770347aff3c6795
Suschevici <ss@1cmd.md>
2023-11-23 00:15:16 +0200

    w4

--- internal/router/handlers/handlers.go
+++ internal/router/handlers/handlers.go
@@ -253,7 +253,15 @@ func (h *handler) GetMetric(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	case storage.Gauge:
-		if _, err = io.WriteString(w, fmt.Sprintf("%.3f", *value.Value)); err != nil {
+		var a []byte
+		if a, err = json.Marshal(*value.Value); err != nil {
+			//if _, err = io.WriteString(w, fmt.Sprintf("%.3f", *value.Value)); err != nil {
+			//if _, err = io.WriteString(w, fmt.Sprintf("%g", *value.Value)); err != nil {
+			//if _, err = io.WriteString(w, fmt.Sprint(*value.Value)); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		if _, err = io.WriteString(w, string(a)); err != nil {
 			//if _, err = io.WriteString(w, fmt.Sprintf("%g", *value.Value)); err != nil {
 			//if _, err = io.WriteString(w, fmt.Sprint(*value.Value)); err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
